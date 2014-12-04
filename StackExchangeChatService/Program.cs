32ae742e3f7ab9eea97627c9b96b2856df2a20c8
ton.yeung <tyeung@auto-master.com>
2014-12-03 22:03:37 -0600

    Added topshelf for easy service install

--- StackExchangeChatService/Program.cs
+++ StackExchangeChatService/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace StackExchangeChatService
 {
@@ -10,6 +11,33 @@ namespace StackExchangeChatService
     {
         static void Main(string[] args)
         {
+            HostFactory.Run(x =>                                 //1
+            {
+                x.Service<TownCrier>(s =>                        //2
+                {
+                    s.ConstructUsing(name => new TownCrier());     //3
+                    s.WhenStarted(tc => tc.Start());              //4
+                    s.WhenStopped(tc => tc.Stop());               //5
+                });
+                x.RunAsLocalSystem();                            //6
+
+                x.SetDescription("Sample Topshelf Host");        //7
+                x.SetDisplayName("Stuff");                       //8
+                x.SetServiceName("stuff");                       //9
+            });                                                  //10
         }
     }
+
+    public class TownCrier
+    {
+        readonly System.Timers.Timer _timer;
+        public TownCrier()
+        {
+            _timer = new System.Timers.Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} an all is well", DateTime.Now);
+        }
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+
 }
