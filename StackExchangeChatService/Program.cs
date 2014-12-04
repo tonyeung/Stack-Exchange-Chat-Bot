5800b22491d0b88335b6d4f57abb54d139a1a451
ton.yeung <tyeung@auto-master.com>
2014-12-03 22:42:21 -0600

    Added WebApi.Client to communicate with stack exchange
    Added HtmlAgilityPack to parse out the documents
    
    Added ability to get FKey

--- StackExchangeChatService/Program.cs
+++ StackExchangeChatService/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Topshelf;
@@ -11,33 +14,78 @@ namespace StackExchangeChatService
     {
         static void Main(string[] args)
         {
-            HostFactory.Run(x =>                                 //1
+            HostFactory.Run(x =>
             {
-                x.Service<TownCrier>(s =>                        //2
+                x.Service<ServiceMain>(s =>
                 {
-                    s.ConstructUsing(name => new TownCrier());     //3
-                    s.WhenStarted(tc => tc.Start());              //4
-                    s.WhenStopped(tc => tc.Stop());               //5
+                    s.ConstructUsing(name => new ServiceMain());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
                 });
-                x.RunAsLocalSystem();                            //6
+                x.RunAsLocalSystem();
 
-                x.SetDescription("Sample Topshelf Host");        //7
-                x.SetDisplayName("Stuff");                       //8
-                x.SetServiceName("stuff");                       //9
-            });                                                  //10
+                x.SetDescription("Stack Exchange Chat Bot");
+                x.SetDisplayName("StackExchangeChatBotService");
+                x.SetServiceName("StackExchangeChatBotService");
+            });
         }
     }
 
-    public class TownCrier
+    public class ServiceMain
     {
-        readonly System.Timers.Timer _timer;
-        public TownCrier()
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FKey { get; set; }
+        public ServiceMain()
         {
-            _timer = new System.Timers.Timer(1000) { AutoReset = true };
-            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} an all is well", DateTime.Now);
+            //todo pull this into app.config
+            Username = "";
+            Password = "";
+            var rawLoginPage = GetLoginPage().Result;
+            FKey = GetValueFromHtmlNode("input", "value", "fkey", rawLoginPage);
         }
-        public void Start() { _timer.Start(); }
-        public void Stop() { _timer.Stop(); }
-    }
 
+        public async Task<string> GetLoginPage()
+        {
+            using (var client = new HttpClient())
+            {
+                // New code:
+                client.BaseAddress = new Uri("https://openid.stackexchange.com/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("account/login");
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    throw new Exception("ERROR getting RawFKey, status: " + response.StatusCode.ToString());
+                }
+            }
+        }
+
+        public static string GetValueFromHtmlNode(string element, string attribute, string name, string html)
+        {
+            try
+            {
+                HtmlDocument htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(html);
+                var nodeFilter = "//" + element + "[@name='" + name + "']";
+                return htmlDocument.DocumentNode.SelectSingleNode(nodeFilter).Attributes[attribute].Value;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Start() { 
+        
+        }
+        public void Stop() { 
+        
+        }
+    }
 }
